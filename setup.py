c536d77dcd29f076fe0a5fd573d5f3028ea43169
an-seunghwan <dpeltms79@gmail.com>
2024-07-29 10:46:40 +0900

    [ADD] automated process

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
+import re
+
+# Read the version from the __init__.py file
+def read_version():
+    with open("distvae_tabular/__init__.py", "r") as f:
+        version_file = f.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+        if version_match:
+            return version_match.group(1)
+        raise RuntimeError("Unable to find version string.")
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read(),
 
 setup(
     name='distvae_tabular',
-    version='0.0.5',
+    version=read_version(),
     author='Seunghwan An',
     author_email='dpeltms79@gmail.com',
     description='DistVAE Implementation Package for Synthetic Data Generation',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/an-seunghwan/DistVAE-Tabular',
     packages=find_packages(),
