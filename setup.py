31da7a41d54902bdd43f3eca2421a153a3e05c04
an-seunghwan <dpeltms79@gmail.com>
2024-12-05 15:26:53 +0900

    Bump version: 0.1.7 → 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
     
 setup(
     name='distvae_tabular',
-    version='0.1.7',
+    version='0.2.0',
     author='Seunghwan An',
     author_email='dpeltms79@gmail.com',
     description='Package for Synthetic Data Generation using Distributional Learninig of VAE',
